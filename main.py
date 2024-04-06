7877b77817ccb9e443367a84dffa2fbdfc05991b
ManiElev <165572223+ManiElev@users.noreply.github.com>
2024-04-06 10:55:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-import kivy.uix.boxlayout
-import kivy.uix.textinput
-import kivy.uix.label
-import kivy.uix.button
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.label import Label
 
+class map(App):
+        def build(self):
+                return Label(text = "Hello")
 
-
-class SimpleApp(kivy.app.App):
- def build(self):
-  self.textInput = kivy.uix.textinput.TextInput()
-  self.label = kivy.uix.label.Label(text="Your Message.")
-  self.button = kivy.uix.button.Button(text="Click Me.")
-  self.button.bind(on_press=self.displayMessage)
-  self.boxLayout = kivy.uix.boxlayout.BoxLayout(orientation="vertical")
-  self.boxLayout.add_widget(self.textInput)
-  self.boxLayout.add_widget(self.label)
-  self.boxLayout.add_widget(self.button)
-  return self.boxLayout
- def displayMessage(self, btn):
-  self.label.text = self.textInput.text
 if __name__ == "__main__":
- simpleApp = SimpleApp()
-simpleApp.run()
+        map().run()
